3471b691b7a1f30c01072e5511b09ec75518bfdc
krishna kumar <krishnakumar@krishnas-MacBook-Pro.local>
2022-08-11 12:45:43 +0530

    ValidateDir

--- validateDir.sh
+++ validateDir.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 echo "To check the directories"
-#echo "AGENT_WORKFOLDER is $AGENT_WORKFOLDER"
+
 ls
-if [ -d "$1" ] 
+if [[ -d "$1" && -f "$1/apiproxy/$1".xml ]]
 then
-    echo "Directory ${1} exists." 
+    echo "The Directory ${1} and the file apiproxy/${1}.xml exists." 
 else
+    echo "The required directory ${1} or file apiproxy/${1}.xml is missing in the repository"
     exit 9999
 fi
+
+
+
+
